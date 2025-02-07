a8fe0f6fe63c2664959f01f041de2ec695feb959
Ahmed-Sayed-1 <ahmed2853519521@gmail.com>
2025-02-07 13:19:03 +0200

    added cart feature

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,6 +10,7 @@ import { DateItemTs } from '../date-item.ts';
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  counter  :number=0;
   proudcts : DateItemTs[]=[];
   proudct1 :{ p: DateItemTs , c : number}[]=[];
   proudct2 :{ id: number , c : number}[]=[];
@@ -19,6 +20,7 @@ export class CartComponent {
   constructor(private counterService: CounterService,  private requestDateApiService: RequestDateApiService) {}
   ngOnInit() {
     this.counterService.getproudcts().subscribe((res) => (this.proudcts = Array.from(res)));
+    this.counterService.getCounter().subscribe((res) => (this.counter = res));
     this.counterService.getproduct().subscribe((res) => {
       res.forEach(item => {
         this.productService.getDetails(item.id.toString()).subscribe(
@@ -41,16 +43,25 @@ export class CartComponent {
     console.log(this.proudcts);
   }
   inc(l: { p: DateItemTs; c: number; }) {
-    // this.counterService.getCounter().subscribe((res) => {
-    //   const counter = res as number;
-    //   console.log(counter);
-      
-    //   this.counterService.setCounter(counter + 1);
-    // });
+    this.counterService.setCounter(this.counter+1);
+    // const product = this.proudct1.find((e) => e.p.id === l.p.id);
+    // if (product) {
+    //   product.c++;
+    // }
+    // const transformedProducts = this.proudct1.map(item => ({ id: item.p.id, counter: item.c }));
+    // this.counterService.setproduct(transformedProducts);
+    
     l.c++;
     this.totalPrice+=l.p.price;
     }
   dec(l: { p: DateItemTs; c: number; }){
+    this.counterService.setCounter(this.counter-1);
+    // const product = this.proudct1.find((e) => e.p.id === l.p.id);
+    // if (product) {
+    //   product.c--;
+    // }
+    // const transformedProducts = this.proudct1.map(item => ({ id: item.p.id, counter: item.c }));
+    // this.counterService.setproduct(transformedProducts);
     l.c--;
     this.totalPrice-=l.p.price;
   }
